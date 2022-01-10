fbd7b1c0646ded62b329df6e6df6399d157a53b0
Rahul Gera <rahulgera05607@gmail.com>
2022-01-10 23:18:16 +0530

    Added Binary Framework

---
+++ RiderTestFramework.xcframework/ios-arm64/RiderTestFramework.framework/Headers/RiderTestFramework.h
@@ -0,0 +1,18 @@
+//
+//  RiderTestFramework.h
+//  RiderTestFramework
+//
+//  Created by Rahul Gera on 29/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RiderTestFramework.
+FOUNDATION_EXPORT double RiderTestFrameworkVersionNumber;
+
+//! Project version string for RiderTestFramework.
+FOUNDATION_EXPORT const unsigned char RiderTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RiderTestFramework/PublicHeader.h>
+
+#import "FirstViewController.h"
---
+++ RiderTestFramework.xcframework/ios-arm64_x86_64-simulator/RiderTestFramework.framework/Headers/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  RiderTestFramework
+//
+//  Created by Rahul Gera on 04/01/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FirstViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
